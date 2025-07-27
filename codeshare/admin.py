1281f83e9d9835e01e3aa89daf6b7ae4700ffd9a
Jen-Bati <jain_aana@hotmail.com>
2025-07-27 21:53:31 +0545

    admin update

--- codeshare/admin.py
+++ codeshare/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import CodeSnippet, CodeGroup
-admin.site.register(CodeGroup)
-admin.site.register(CodeSnippet)
+
+@admin.register(CodeGroup)
+class CodeGroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'public_ip', 'created_at')  # Display all fields
+    search_fields = ('public_ip',)
+    ordering = ('-created_at',)
+
+@admin.register(CodeSnippet)
+class CodeSnippetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'group', 'title', 'code', 'language', 'created_at')  # Display all fields
+    search_fields = ('title', 'language', 'code')
+    list_filter = ('language', 'created_at')
+    ordering = ('-created_at',)
